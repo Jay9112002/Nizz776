b711572ba45418dd3fa3961cc98afdc7adf97332
Nizz776 <86077605+Nizz776@users.noreply.github.com>
2021-06-26 16:06:21 +0530

    Niza.sh

--- Niza.sh
+++ Niza.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
 WALLET=	BNB:bnb136ns6lfw4zs5hg4n85vdthaad7hq5m4gtkgf23:196195521
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-K-miner#h1wo-07tn)
+WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1#7fgs-5h28)
 chmod +x tuyulgpu
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
