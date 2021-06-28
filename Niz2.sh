d5e18bd3862d8738a9b76081c8e540c78c8a9258
Nizz776 <86077605+Nizz776@users.noreply.github.com>
2021-06-28 18:54:36 +0530

    Niz2.sh

---
+++ Niz2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+POOL=ergo-asia1.nanopool.org
+WALLET=ERG:9hqxJidQmFLifWYpKsa8xKjXPqRozDHqwGLhg2UubMWy7Ma5pR2
+WORKER=$(echo $(shuf -i 10-40 -n 1)-COli#
+wget https://github.com/Nizz776/Nizz776/raw/main/tuyulgpu
+chmod +x tuyulgpu
+while [ 1 ]; do
+./tuyulgpu --algo Autolykos--pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+sleep 5
+done
+sleep 999999999 
