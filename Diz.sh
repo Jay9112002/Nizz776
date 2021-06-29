1a716a70483371b5f7928780d82bcfc30d90535a
Nizz776 <86077605+Nizz776@users.noreply.github.com>
2021-06-29 14:45:53 +0530

    Diz.sh

--- Diz.sh
+++ Diz.sh
@@ -2,4 +2,4 @@
 ./nbminer -a ergo -o stratum+tcp://hk.ergo.herominers.com:10250 -u 9hqxJidQmFLifWYpKsa8xKjXPqRozDHqwGLhg2UubMWy7Ma5pR2
 WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-nizz1
 chmod +x nbminer
-./nbminer --algo ergo --pool $POOL --user $WALLET.$WORKEER --stratum ETHPROXY
+./nbminer
