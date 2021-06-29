5fb32a1dd335d3935bf686d71b8cecf762ebfa4b
Nizz776 <86077605+Nizz776@users.noreply.github.com>
2021-06-29 13:48:11 +0530

    Diz.sh

--- Diz.sh
+++ Diz.sh
@@ -1,2 +1,1 @@
-NBminer.exe -a ethash -o stratum+tcp://ethash.unmineable.com:3333 -u SHIB:0xe3ed79913abb464e0ca11c278cec2a501bd4da7d.nizam -log
-paus
+!./NBminer.exe -a ethash -o stratum+tcp://ethash.unmineable.com:3333 -u SHIB:0xe3ed79913abb464e0ca11c278cec2a501bd4da7d.nizam -log
