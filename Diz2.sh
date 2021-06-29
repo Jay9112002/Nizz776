975473295b5e726d6e01a9b3f1f89e28eab6b609
Nizz776 <86077605+Nizz776@users.noreply.github.com>
2021-06-29 20:44:52 +0530

    Diz2.sh

--- Diz2.sh
+++ Diz2.sh
@@ -5,4 +5,4 @@ WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-nizz1#w3o2-bj87)
 
 cd "$(dirname "$0")"
 
-chmod +x ./nbminer && sudo ./nbminer -a ETHASH -o $POOL -u $WALLET.$WORKER $@
+chmod +x ./nbminer && sudo ./nbminer -a ethash o $POOL -u $WALLET.$WORKER $@
