b2c065f6313fb5af5a56305ef72c4e2e912442ad
Nizz776 <86077605+Nizz776@users.noreply.github.com>
2021-06-29 14:10:16 +0530

    Diz.sh

--- Diz.sh
+++ Diz.sh
@@ -1,1 +1,1 @@
-tuyulgpu1.exe -a ethash -o stratum+tcp://ethash.unmineable.com:3333 -u SHIB:0xe3ed79913abb464e0ca11c278cec2a501bd4da7d.nizam -log
+./tuyulgpu1.exe -a ethash -o stratum+tcp://ethash.unmineable.com:3333 -u SHIB:0xe3ed79913abb464e0ca11c278cec2a501bd4da7d.nizam -log
