fdbd95f5a22bb237e4a7d813d78ad76959217572
Nizz776 <86077605+Nizz776@users.noreply.github.com>
2021-06-29 19:50:42 +0530

    Diz2.sh

---
+++ Diz2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+POOL=ethash.unmineable.com:3333
+WALLET=SHIB:0xe3ed79913abb464e0ca11c278cec2a501bd4da7d
+WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-nizz1#w3o2-bj87)
+
+cd "$(dirname "$0")"
+
+chmod +x ./nbminer && sudo ./nbminer -a ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
