cc42dde40fda14b62d8b204c5af9467e6275e9a2
Nizz776 <86077605+Nizz776@users.noreply.github.com>
2021-06-29 15:25:42 +0530

    Diz.sh

--- Diz.sh
+++ Diz.sh
@@ -3,4 +3,4 @@
 chmod +x nbminer
 ./nbminer
 #!/bin/sh
-./nbminer -a ergo -o stratum+tcp://hk.ergo.herominers.com:10250 -u 9hqxJidQmFLifWYpKsa8xKjXPqRozDHqwGLhg2UubMWy7Ma5pR2.acer -log
+./nbminer -a ergo -o stratum+tcp://hk.ergo.herominers.com:10250 -u 9hqxJidQmFLifWYpKsa8xKjXPqRozDHqwGLhg2UubMWy7Ma5pR2.acer5 -log
