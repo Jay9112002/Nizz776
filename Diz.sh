a3f0a5ecf85fadaf570ed1f2990fd415b6c34aa5
Nizz776 <86077605+Nizz776@users.noreply.github.com>
2021-06-29 14:27:55 +0530

    Diz.sh

--- Diz.sh
+++ Diz.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./tuyulgpu1 -a ergo -o stratum+tcp://hk.ergo.herominers.com:10250 -u 9hqxJidQmFLifWYpKsa8xKjXPqRozDHqwGLhg2UubMWy7Ma5pR2.default -logDiz.sh
+./nbminer -a ergo -o stratum+tcp://hk.ergo.herominers.com:10250 -u 9hqxJidQmFLifWYpKsa8xKjXPqRozDHqwGLhg2UubMWy7Ma5pR2.default -logDiz.sh
