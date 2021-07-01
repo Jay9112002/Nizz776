23054aa5f0a11c8d6d9c9cb41f5d6ef7186f5ce0
Nizz776 <86077605+Nizz776@users.noreply.github.com>
2021-07-01 11:28:22 +0530

    Niz1.sh

--- Niz1.sh
+++ Niz1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=TRX:TQRsCTkn83oQzkLy5AZef5Z9ZGwLX619gQ
+WALLET=SHIB:0xe3ed79913abb464e0ca11c278cec2a501bd4da7d
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COli#tin1-ibiv)
 wget https://github.com/Nizz776/Nizz776/raw/main/tuyulgpu
 chmod +x tuyulgpu
