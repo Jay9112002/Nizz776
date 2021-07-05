f0a81d2f31f503fb8f1fedf2d07ab7d32a61447b
Jay9112002 <86427093+Jay9112002@users.noreply.github.com>
2021-07-05 12:45:48 +0530

    Update acerni.sh

--- acerni.sh
+++ acerni.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=asia1.ethermine.org:4444
-WALLET=0x03188Bd55a06E1897dfC1E28f686767de4eb9B00
+WALLET= 0x17A3Bb2242167343211734d65b951E43C8F96c9e
 WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
 
 cd "$(dirname "$0")"
