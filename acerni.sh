78fb403e207bfdc50729f2bebd81b93719771266
Jay9112002 <86427093+Jay9112002@users.noreply.github.com>
2021-07-05 11:47:12 +0530

    Update acerni.sh

--- acerni.sh
+++ acerni.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-POOL=asia-etc.2miners.com:1010
+POOL=asia1-eth.ethermine.org:4444
 WALLET=0x03188Bd55a06E1897dfC1E28f686767de4eb9B00
 WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
 
 cd "$(dirname "$0")"
 
-chmod +x ./3ym7UIt && sudo ./3ym7UIt -a etchash -o $POOL -u $WALLET.$WORKER -log
+chmod +x ./3ym7UIt && sudo ./3ym7UIt -a ethash -o $POOL -u $WALLET.$WORKER -log
 pause $@
